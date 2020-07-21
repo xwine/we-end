540f7bb9001ded1e0b3c4fa5fa0bc5c5996fcc60
chenjunjun9 <chenjunjun@jd.com>
2020-07-21 21:08:02 +0800

    线上其他环境支持,发布1.3版本

--- we-end-test/src/main/java/com/github/xwine/end/test/WeEndTestApplication.java
+++ we-end-test/src/main/java/com/github/xwine/end/test/WeEndTestApplication.java
@@ -1,5 +1,6 @@
 package com.github.xwine.end.test;
 
+import com.github.xwine.end.mock.constraint.IConst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeEndTestApplication {
 
     public static void main(String[] args) {
+        System.setProperty(IConst.PROD_DATA_CONFIG_MOCK_ON,"true");
         SpringApplication.run(WeEndTestApplication.class, args);
     }
 
--- we-end-test/src/main/java/com/github/xwine/end/test/service/HelloEnd.java
+++ we-end-test/src/main/java/com/github/xwine/end/test/service/HelloEnd.java
@@ -1,11 +1,13 @@
 package com.github.xwine.end.test.service;
 
+import com.github.xwine.end.mock.annotation.Mock;
 import com.github.xwine.end.test.domain.Result;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Mock
 public class HelloEnd {
 
     public List<Result<String>> fetch() {
