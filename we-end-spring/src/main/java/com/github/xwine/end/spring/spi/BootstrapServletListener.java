80ac948cfb47b2beb7408a6296fef2d11388c208
chenjunjun9 <chenjunjun@jd.com>
2020-07-22 12:37:41 +0800

    filter重复加载bugfix

--- we-end-spring/src/main/java/com/github/xwine/end/spring/spi/BootstrapServletListener.java
+++ we-end-spring/src/main/java/com/github/xwine/end/spring/spi/BootstrapServletListener.java
@@ -4,9 +4,11 @@ import com.github.xwine.end.spring.WebEndFilter;
 import com.github.xwine.end.mock.MockContext;
 import org.springframework.web.WebApplicationInitializer;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.util.EnumSet;
 
 /**
  * 利用servlet3提供的spi自动注入
@@ -21,8 +23,10 @@ public class BootstrapServletListener implements WebApplicationInitializer {
         }
         if (MockContext.getConfig().getMockOn()) {
             FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("webEndFilter", new WebEndFilter());
-            filterRegistration.addMappingForUrlPatterns(null, false, MockContext.getConfig().getConsolePrefix()+"/*");
-            filterAppended = true;
+            if (filterRegistration != null) {
+                filterRegistration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, MockContext.getConfig().getConsolePrefix() + "/*");
+                filterAppended = true;
+            }
         }
     }
 }
